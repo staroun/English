d6606c83229215642ebcb6cd6b540cf084d0180c
Sungwoon Lee <staroun@gmail.com>
2022-10-27 10:00:49 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,43 +20,23 @@ import TableCell from '@mui/material/TableCell';
 //   }
 // })
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '이성운',
-    'birthday': "111111",
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '홍길동',
-    'birthday': "222222",
-    'gender': '남자',
-    'job': '회사원'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '김미라',
-    'birthday': "333333",
-    'gender': '여자',
-    'job': '대학생'
-  },
-  {
-    'id': 4,
-    'image': 'https://placeimg.com/64/64/4',
-    'name': '김순',
-    'birthday': "444444",
-    'gender': '여자',
-    'job': '부동산업'
-  }
-]
-
-
 class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const reponse = await fetch('/api/customers');
+    const body = await reponse.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -73,7 +53,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) })}
+            {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) }) : ""}
           </TableBody>
         </Table>
       </Paper>
